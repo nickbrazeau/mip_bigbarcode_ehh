9d423b3cb3ec5a0d5c81f2b8da53a69304fb1374
Nicholas Brazeau <nbrazeau@med.unc.edu>
2019-01-29 20:34:01 -0500

    push

---
+++ R/00_functions_temp_if_PR.R
@@ -0,0 +1,63 @@
+
+#.................................
+# Functions
+#.................................
+
+
+#' @title Assign the GT calls from the NONREFERENT WSAF
+#' @return A GT matrix
+#' not exported
+
+assignGTfromWSNRAF<- function(wsnraf, cutoff = 0.1){
+
+  GT <- matrix(NA, dim(wsnraf)[1], dim(wsnraf)[2])
+
+  GT <- ifelse(wsnraf > 1-cutoff, "1/1",
+               ifelse(wsnraf < 0+cutoff, "0/0",
+                      ifelse(!is.na(wsnraf), "0/1", NA)))
+
+  return(GT)
+
+}
+
+
+#' @title Convert a MIPanalyzer Biallelic Object to a vcfR object
+#' @param input is of class \code{mipanalyzer_biallelic}
+#' @param cutoff is the within-sample non-referent allele frequency cutoff to transform your biallelic site to a genotype matrix.
+#'
+#' @export
+
+
+MIPanalyzerbi2vcfR <- function(input = NULL, cutoff = cutoff){
+
+  if(!inherits(input, c("mipanalyzer_biallelic"))){
+    stop("This function only works on objects of class mipanalyzer_biallelic or mipanalyzer_multiallelic, not class ", class(mipobj))
+  }
+
+  # setup for gt
+  wsnraf <- input$counts/input$coverage
+  GT <- assignGTfromWSNRAF(wsnraf, cutoff = cutoff)
+  ADref <- input$counts
+  ADalt <- input$coverage - input$counts
+  DP <- input$coverage
+
+  gt <- t( ifelse(is.na(GT), NA, paste0(GT, ":", ADref, ",", ADalt, ":", DP)) ) # loci as rows, smpls as columns
+
+  # append format column and sample names
+  gt <- cbind(FORMAT = "GT:AD:DP", gt)
+  colnames(gt)[2:ncol(gt)] <- input$samples$ID
+
+  # getFix
+  fix <- as.matrix(input$loci[,c("CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO")]) # must be in this order and only these
+  # https://stackoverflow.com/questions/15618527/why-does-as-matrix-add-extra-spaces-when-converting-numeric-to-character
+  # not sure why trim is failing here. annoying
+  fix[,2] <- stringr::str_replace(fix[,2], "\\s", "")
+  # get meta
+  # meta <- append(input$vcfmeta, "##MIPanalyzer=This vcf was filtered and modified by the MIPanalyzer R package")
+  meta <- c("##fileformat=VCFv4.2", "##Simulated with vcfR2manip make_bifastamatvcfR")
+  # write out new vcfRobj
+  newvcfR <- new("vcfR", meta = meta, fix = fix, gt = gt)
+  return(newvcfR)
+
+}
+
